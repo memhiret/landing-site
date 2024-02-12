dd3645ed3243bfb7663660d2fdd395994dc37af3
yonit1dev <yonit.anosie@gmail.com>
2024-02-12 17:08:49 +0300

    Fixes to Deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: true,
   css: ['~/assets/css/global.css', '~/assets/css/button.css', '~/assets/css/mytabs.css'],
   modules: ['nuxt-svgo'],
   svgo: {
